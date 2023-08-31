50ae8721b7cbea2cdb718a0877e3b42a101e591b
Piotr Wojda <piotr.wojda.1999@gmail.com>
2023-08-31 17:13:09 +0200

    API full

--- test_server.py
+++ test_server.py
@@ -1,3 +1,20 @@
 import requests
 
-print(requests.get("http://127.0.0.1:8000/").json())
+
+def main():
+    print(f"""All printed items:\n{requests.get("http://127.0.0.1:8000/").json()}""")
+    print(f"""Item ID 1:\n{requests.get("http://127.0.0.1:8000/items/1").json()}""")
+    print(
+        f"""Item ID 44:\n{requests.get("http://127.0.0.1:8000/items/44").json()}"""
+    )  # not found - raised error
+
+    print(f"""\nItems:\n{requests.get("http://127.0.0.1:8000/items").json()}""")
+
+    print("\n\nUpdating an item:")
+    print(requests.put("http://127.0.0.1:8000/update/0?count=9001").json())
+    print(requests.get("http://127.0.0.1:8000/").json())
+    print()
+
+
+if __name__ == "__main__":
+    main()
